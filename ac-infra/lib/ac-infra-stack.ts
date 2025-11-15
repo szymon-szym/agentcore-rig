ebfcfcdf80773a4583cb995d8a3ab062328419d4
szymon-szym <szymon.mich@gmail.com>
2025-11-15 14:40:37 +0100

    add action

--- ac-infra/lib/ac-infra-stack.ts
+++ ac-infra/lib/ac-infra-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as iam from "aws-cdk-lib/aws-iam";
+
 export class AcInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -8,5 +10,13 @@ export class AcInfraStack extends cdk.Stack {
     const acRepository = new ecr.Repository(this, "ACRepository", {
       repositoryName: "ac-repository",
     });
+
+    // user to be used by GH actions
+    const ghActionsUser = new iam.User(this, "GHActionsUser", {
+      userName: "gh-actions-user",
+    });
+
+    // grant permissions to push to ECR
+    acRepository.grantPush(ghActionsUser);
   }
 }
