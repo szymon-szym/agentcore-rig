d4a99d62c552f86f98f34a8c3e36b4eb4f5fdf62
szymon-szym <szymon.mich@gmail.com>
2025-11-16 13:56:30 +0100

    add rig

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,9 +1,17 @@
 use axum::{
     Json, Router, debug_handler,
+    extract::State,
     routing::{get, post},
 };
+use rig::{client::completion::CompletionClientDyn, completion::Prompt};
+use rig_bedrock::{
+    client::{Client, ClientBuilder},
+    completion::AMAZON_NOVA_PRO,
+};
+
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
+use tracing::info;
 
 #[derive(Serialize)]
 pub struct StatusResponse {
@@ -21,11 +29,25 @@ pub struct InvocationsResponse {
     message: String,
 }
 
+#[derive(Debug, Clone)]
+pub struct AppState {
+    rig_client: Client,
+}
+
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let client = ClientBuilder::new().region("us-east-1").build().await;
+
+    let state = AppState {
+        rig_client: client.clone(),
+    };
+
     let app = Router::new()
         .route("/ping", get(ping))
-        .route("/invocations", post(invocations));
+        .route("/invocations", post(invocations))
+        .with_state(state);
 
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -39,11 +61,20 @@ async fn ping() -> Json<StatusResponse> {
     Json(response)
 }
 
-async fn invocations(Json(payload): Json<InvocationsRequest>) -> Json<InvocationsResponse> {
+async fn invocations(
+    State(state): State<AppState>,
+    Json(payload): Json<InvocationsRequest>,
+) -> Json<InvocationsResponse> {
     let prompt = payload.prompt;
 
-    let echo = format!("echo {}", prompt);
+    let agent = state
+        .rig_client
+        .agent(AMAZON_NOVA_PRO)
+        .preamble("You are helpful assistant. Respond only with the answer. Be concise.")
+        .build();
 
-    let response = InvocationsResponse { message: echo };
+    let response = agent.prompt(prompt).await.unwrap();
+
+    let response = InvocationsResponse { message: response };
     Json(response)
 }
